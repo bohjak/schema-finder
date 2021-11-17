2c84088aad1ab8a53730107f51b66525a1d263a6
jamesbohacek <jamesbohacek@outlook.com>
2021-11-17 10:38:16 +0100

    chore: format everything in src

--- src/stories/Finder.stories.tsx
+++ src/stories/Finder.stories.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import { Meta } from '@storybook/react';
-import {Finder} from '../internal';
-import fds from './assets/fds.json';
-import i18n from './assets/i18n.json';
-import config from './assets/config.json';
+import {Meta} from "@storybook/react";
+import React from "react";
+import {Finder} from "../internal";
+import config from "./assets/config.json";
+import fds from "./assets/fds.json";
+import i18n from "./assets/i18n.json";
 
 export default {
   component: Finder,
-  title: 'Schemas/Finder'
+  title: "Schemas/Finder",
 } as Meta;
 
-export const SingleSchema: React.VFC = () => <Finder schemas={{'fds': fds as any}} />
+export const SingleSchema: React.VFC = () => (
+  <Finder schemas={{fds: fds as any}} />
+);
 
-export const MultipleSchemas: React.VFC = () => <Finder schemas={{'fds': fds as any, 'i18n': i18n as any, 'config': config as any}} />
+export const MultipleSchemas: React.VFC = () => (
+  <Finder
+    schemas={{fds: fds as any, i18n: i18n as any, config: config as any}}
+  />
+);
