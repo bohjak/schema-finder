fd9c23a9ff12e69efe4a9c4b21471902b721137f
jamesbohacek <jamesbohacek@outlook.com>
2022-01-18 16:43:30 +0100

    chore: print build metafile

--- build.mjs
+++ build.mjs
@@ -1,12 +1,21 @@
-import { build } from 'esbuild';
+// @ts-check
+import {build, analyzeMetafile} from "esbuild";
+import {performance} from "perf_hooks";
 
+const start = performance.now();
 build({
-  entryPoints: ['src/index.ts'],
+  entryPoints: ["src/index.ts"],
   bundle: true,
   minify: true,
   sourcemap: true,
-  outdir: 'dist',
-  platform: 'node',
-  target: 'node14',
-  external: ['react', 'react-dom', 'styled-components']
-});
+  outdir: "dist",
+  platform: "node",
+  target: "node14",
+  external: ["react", "react-dom", "styled-components"],
+  metafile: true,
+})
+  .then(({metafile}) => analyzeMetafile(metafile))
+  .then(console.log)
+  .finally(() =>
+    console.log(`⚡  Built in ${Math.round(performance.now() - start)}ms.`)
+  );
